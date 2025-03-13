ade11c1c0774099c6c6324ed997784fa5d71c2b3
Sanika Peshkar <sanikapeshkar480@gmail.com>
2025-03-13 23:00:07 +0530

    feat : integrated Next/auth github login

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,5 +2,5 @@ import { Navbar } from "@/components/Navbar/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <>   <Navbar/></>;
+  return <h1>Home</h1>;
 }
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,46 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
 import Link from "next/link";
 
-export const Navbar = () => {
+export const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <header className="px-2 py-5 bg-white shadow-sm font-work-sans">
+    <header className="px-2 py-2 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={54} height={10} />
         </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button
+                onClick={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                Signout
+              </button>
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn();
+              }}
+            >
+              <button type="submit">SignIn</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
